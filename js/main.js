dccef99005ef34d1153412868345bf7104bf7557
goraesaim <goraesaim@gmail.com>
2024-11-07 22:35:01 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,143 @@
+// 검색
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+
+//스크롤 배지
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
+  }
+},300));
+
+toTopEl.addEventListener('click', function() {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
+
+
+// 순서대로 나타나기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    opacity: 1,
+    delay: (index + 1) * .7
+  })
+});
+
+
+// 슬라이드 요소 관리
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+})
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3,  // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10,  // 슬라이드 사이 여백
+  centeredSlides: true,  // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: { // 페이지 번호 사용 여부
+    el: '.promotion .swiper-pagination', // 페이비 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    nextEl: '.promotion .swiper-next', // 다음 버튼 선택자
+    prevEl: '.promotion .swiper-prev' // 이전 버튼 선택자
+  }
+})
+new Swiper('.awards .swiper', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+})
+
+
+// Promotion 슬라이드 토글 기능
+const promotionEl = document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion) {
+    promotionEl.classList.add('hide')
+  } else {
+    promotionEl.classList.remove('hide')
+  }
+})
+
+
+// 부유하는 요소 관리
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, //선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      delay: random(0, delay),
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut"
+  })
+}
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
+
+
+//  요소가 화면에 보여짐 여부에 다른 관리
+const spyEls = document.querySelectorAll('section.scroll-spy') //관리할 요소들 검색
+//  요소들 반복 처리
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({  // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl,  // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8  // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show')  // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller())  // 컨트롤러에 장면을 할당(필수!!)
+})
