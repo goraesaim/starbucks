0d30f6cacf9003c1ed70694cbdc66004d4433c25
goraesaim <goraesaim@gmail.com>
2024-11-09 01:52:41 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// 검색
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 //스크롤 배지
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
